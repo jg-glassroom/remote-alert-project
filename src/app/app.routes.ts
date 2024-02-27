a658c77e2a19583983bfd224ed9b5b7aa24e1a46
Raoof Rachidi <rrachidi@HMNS-MTL-545.local>
2024-02-27 12:53:58 -0500

    Add alerts component and menu sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
+
 import { TableComponent } from './components/table/table.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { AlertsComponent } from './components/alerts/alerts.component';
+
 import { loggedInGuard } from './guards/logged-in/logged-in.guard';
 import { notLoggedInGuard } from './guards/not-logged-in/not-logged-in.guard';
 
 
 export const routes: Routes = [
     { path: 'home', component: TableComponent, canActivate: [loggedInGuard] },
+    { path: 'alerts', component: AlertsComponent, canActivate: [loggedInGuard] },
     { path: '', component: SignInComponent, canActivate: [notLoggedInGuard] },
 ];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,15 +4,31 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule],
+  imports: [
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatListModule, 
+    MatSidenavModule, 
+    CommonModule, 
+    RouterOutlet
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, private router: Router) {}
+
+  public navigate(route: any) {
+    this.router.navigate([route])
+  }
 }
