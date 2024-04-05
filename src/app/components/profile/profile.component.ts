2685983cf44e70bbeaf3278090b287201199614f
Raoof Rachidi <rrachidi@HMNS-MTL-545.local>
2024-04-05 14:51:53 -0400

    Add accordion for scopes and plaforms service

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -12,6 +12,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireFunctions } from '@angular/fire/compat/functions';
@@ -21,12 +22,13 @@ import { ChangePasswordComponent } from '../change-password/change-password.comp
 
 import { AuthService } from '../../services/auth.service';
 import { ExternalPlatformsService } from '../../services/external-platforms.service';
+import { GoogleService } from '../../services/platforms/google/google.service';
+import { FacebookService } from '../../services/platforms/facebook/facebook.service';
 
 import { first, switchMap } from 'rxjs/operators';
 import { throwError, firstValueFrom } from 'rxjs';
 
 import { getAuth } from 'firebase/auth';
-import { getFirestore, doc, updateDoc, deleteField } from '@firebase/firestore';
 
 import { ToastrService } from 'ngx-toastr';
 
@@ -45,7 +47,8 @@ import { ToastrService } from 'ngx-toastr';
     MatCardModule,
     MatSelectModule,
     MatTabsModule,
-    MatIconModule
+    MatIconModule,
+    MatExpansionModule
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
@@ -53,10 +56,7 @@ import { ToastrService } from 'ngx-toastr';
 export class ProfileComponent {
   profileForm!: FormGroup;
   toaster = inject(ToastrService);
-  DV360Metrics: boolean = false;
-  DV360Config: boolean = false;
-  searchAds: boolean = false;
-  CM360: boolean = false;
+  panelOpenState = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -65,7 +65,9 @@ export class ProfileComponent {
     public externalPlatformService: ExternalPlatformsService,
     public dialog: MatDialog,
     private route: ActivatedRoute,
-    private fns: AngularFireFunctions
+    private fns: AngularFireFunctions,
+    public googleService: GoogleService,
+    public facebookService: FacebookService
   ) {}
 
   ngOnInit(): void {
@@ -73,40 +75,7 @@ export class ProfileComponent {
     this.getUserProfile();
     this.handleQueryParams();
     if (this.isConnected('google')) {
-      this.getGoogleScopes();
-    }
-  }
-
-  async getGoogleScopes(retryCount = 2): Promise<void> {
-    const tokenInfoUrl = `https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${localStorage.getItem('googleAccessToken')}`;
-    try {
-      const response = await fetch(tokenInfoUrl);
-      if (!response.ok) {
-        const error = new Error(`HTTP error: ${response.status}`);
-        (error as any).status = response.status;
-        throw error;
-      }
-      const data = await response.json();
-      const googleScopes = data.scope.split(' ');
-      if (googleScopes.includes('https://www.googleapis.com/auth/doubleclicksearch')) {
-        this.searchAds = true;
-      }
-      if (googleScopes.includes('https://www.googleapis.com/auth/dfareporting')) {
-        this.CM360 = true;
-      }
-      if (googleScopes.includes('https://www.googleapis.com/auth/doubleclickbidmanager')) {
-        this.DV360Metrics = true;
-      }
-      if (googleScopes.includes('https://www.googleapis.com/auth/display-video')) {
-        this.DV360Config = true;
-      }
-    } catch (error: any) {
-      if (retryCount > 0) {
-        await this.externalPlatformService.handleGoogleError(error);
-        return this.getGoogleScopes(retryCount - 1);
-      } else {
-          this.toaster.error('Error verifying token info', 'Error');
-      }
+      this.googleService.getGoogleScopes();
     }
   }
 
@@ -224,72 +193,4 @@ export class ProfileComponent {
       width: '350px',
     });
   }
-
-  googleConnect() {
-    var clientId = '552619214593-phjqlsgv1kqkq2nadui8rsuknjudo9lv.apps.googleusercontent.com';
-    var hostname = window.location.hostname;
-    var redirectUri = hostname === "localhost" ? 
-    'http://localhost:4200/profile' : 'https://alert-project-xy52mshrpa-nn.a.run.app/profile';
-    var scope = 'profile email https://www.googleapis.com/auth/display-video https://www.googleapis.com/auth/doubleclickbidmanager https://www.googleapis.com/auth/dfareporting https://www.googleapis.com/auth/doubleclicksearch';
-
-    var authUrl = 'https://accounts.google.com/o/oauth2/v2/auth?' +
-      'client_id=' + clientId +
-      '&response_type=code' +
-      '&scope=' + scope +
-      '&redirect_uri=' + redirectUri +
-      '&access_type=offline' +
-      '&prompt=consent';
-
-    window.location.href = authUrl;
-  }
-
-  async googleDisconnect(retryCount = 2): Promise<any> {
-    const accessToken = localStorage.getItem('googleAccessToken');
-
-    if (accessToken) {
-      try {
-        const response = await fetch(`https://accounts.google.com/o/oauth2/revoke?token=${accessToken}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-        });
-  
-        if (!response.ok) {
-          throw new Error('Failed to revoke token');
-        }
-      } catch (error: any) {
-        if (retryCount > 0) {
-          await this.externalPlatformService.handleGoogleError(error);
-          return this.googleDisconnect(retryCount - 1);
-        } else {
-            this.toaster.error(`Error revoking Google token: ${error}`, 'Error');
-        }
-      }
-    }
-
-    localStorage.removeItem('googleAccessToken');
-
-    const db = getFirestore();
-    const auth = getAuth();
-    const user = auth.currentUser;
-
-    if (user) {
-      try {
-        await updateDoc(doc(db, "user", user.uid), {
-          googleRefreshToken: deleteField(),
-          googleAccessToken: deleteField()
-        });
-        this.toaster.success("Google has been successfully disconnected", "Success");
-      } catch (error) {
-        this.toaster.error(`Error deleting tokens in Firestore: ${error}`, "Error");
-      }
-    } else {
-      console.log("User not logged in");
-    }
-  }  
-
-  facebookConnect() {}
-
-  facebookDisconnect() {}
 }
---
+++ src/app/services/platforms/google/google.service.ts
@@ -0,0 +1,118 @@
+import { Injectable, inject } from '@angular/core';
+
+import { ToastrService } from 'ngx-toastr';
+
+import { getFirestore, doc, updateDoc, deleteField } from '@firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+import { ExternalPlatformsService } from '../../external-platforms.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleService {
+  toaster = inject(ToastrService);
+  DV360Metrics: boolean = false;
+  DV360Config: boolean = false;
+  searchAds: boolean = false;
+  CM360: boolean = false;
+
+  constructor(public externalPlatformService: ExternalPlatformsService) { }
+
+  googleConnect() {
+    var clientId = '552619214593-phjqlsgv1kqkq2nadui8rsuknjudo9lv.apps.googleusercontent.com';
+    var hostname = window.location.hostname;
+    var redirectUri = hostname === "localhost" ? 
+    'http://localhost:4200/profile' : 'https://alert-project-xy52mshrpa-nn.a.run.app/profile';
+    var scope = 'profile email https://www.googleapis.com/auth/display-video https://www.googleapis.com/auth/doubleclickbidmanager https://www.googleapis.com/auth/dfareporting https://www.googleapis.com/auth/doubleclicksearch';
+
+    var authUrl = 'https://accounts.google.com/o/oauth2/v2/auth?' +
+      'client_id=' + clientId +
+      '&response_type=code' +
+      '&scope=' + scope +
+      '&redirect_uri=' + redirectUri +
+      '&access_type=offline' +
+      '&prompt=consent';
+
+    window.location.href = authUrl;
+  }
+
+  async googleDisconnect(retryCount = 2): Promise<any> {
+    const accessToken = localStorage.getItem('googleAccessToken');
+
+    if (accessToken) {
+      try {
+        const response = await fetch(`https://accounts.google.com/o/oauth2/revoke?token=${accessToken}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+        });
+  
+        if (!response.ok) {
+          throw new Error('Failed to revoke token');
+        }
+      } catch (error: any) {
+        if (retryCount > 0) {
+          await this.externalPlatformService.handleGoogleError(error);
+          return this.googleDisconnect(retryCount - 1);
+        } else {
+            this.toaster.error(`Error revoking Google token: ${error}`, 'Error');
+        }
+      }
+    }
+
+    localStorage.removeItem('googleAccessToken');
+
+    const db = getFirestore();
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user) {
+      try {
+        await updateDoc(doc(db, "user", user.uid), {
+          googleRefreshToken: deleteField(),
+          googleAccessToken: deleteField()
+        });
+        this.toaster.success("Google has been successfully disconnected", "Success");
+      } catch (error) {
+        this.toaster.error(`Error deleting tokens in Firestore: ${error}`, "Error");
+      }
+    } else {
+      console.log("User not logged in");
+    }
+  }
+
+  async getGoogleScopes(retryCount = 2): Promise<void> {
+    const tokenInfoUrl = `https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${localStorage.getItem('googleAccessToken')}`;
+    try {
+      const response = await fetch(tokenInfoUrl);
+      if (!response.ok) {
+        const error = new Error(`HTTP error: ${response.status}`);
+        (error as any).status = response.status;
+        throw error;
+      }
+      const data = await response.json();
+      const googleScopes = data.scope.split(' ');
+      if (googleScopes.includes('https://www.googleapis.com/auth/doubleclicksearch')) {
+        this.searchAds = true;
+      }
+      if (googleScopes.includes('https://www.googleapis.com/auth/dfareporting')) {
+        this.CM360 = true;
+      }
+      if (googleScopes.includes('https://www.googleapis.com/auth/doubleclickbidmanager')) {
+        this.DV360Metrics = true;
+      }
+      if (googleScopes.includes('https://www.googleapis.com/auth/display-video')) {
+        this.DV360Config = true;
+      }
+    } catch (error: any) {
+      if (retryCount > 0) {
+        await this.externalPlatformService.handleGoogleError(error);
+        return this.getGoogleScopes(retryCount - 1);
+      } else {
+          this.toaster.error('Error verifying token info', 'Error');
+      }
+    }
+  }
+}
