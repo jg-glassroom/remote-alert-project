f03929076117c65a32074d94f2060a146194277f
Andres Ponce de Leon <aponcedeleon@HMNS-MTL-286.local>
2024-04-15 16:54:51 -0400

    UI design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { AlertsComponent } from './components/alerts/alerts.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { PacingTemplateComponent } from './components/pacing-template/pacing-template.component';
 
 import { loggedInGuard } from './guards/logged-in/logged-in.guard';
 import { notLoggedInGuard } from './guards/not-logged-in/not-logged-in.guard';
@@ -13,5 +14,6 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent, canActivate: [loggedInGuard] },
     { path: 'alerts', component: AlertsComponent, canActivate: [loggedInGuard] },
     { path: 'profile', component: ProfileComponent, canActivate: [loggedInGuard] },
+    { path: 'pacing', component: PacingTemplateComponent, canActivate: [loggedInGuard] },
     { path: '', component: SignInComponent, canActivate: [notLoggedInGuard] },
 ];
