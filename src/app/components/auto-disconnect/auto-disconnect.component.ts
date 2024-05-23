6a330d9dae6e6af9c7033f8986fa2cd147866e0f
Raoof Rachidi <rrachidi@HMNS-MTL-545.local>
2024-05-23 09:31:05 -0400

    fix idle

---
+++ src/app/components/auto-disconnect/auto-disconnect.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auto-disconnect',
+  standalone: true,
+  imports: [],
+  templateUrl: './auto-disconnect.component.html',
+  styleUrl: './auto-disconnect.component.css'
+})
+export class AutoDisconnectComponent {
+
+}
