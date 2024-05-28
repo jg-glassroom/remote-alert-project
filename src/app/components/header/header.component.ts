3ddb8ff3b75a55883c4b6c1941141e845063ce89
Raoof Rachidi <rrachidi@HMNS-MTL-545.local>
2024-05-28 15:15:55 -0400

    Fix selectAccount

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -240,7 +240,7 @@ export class HeaderComponent {
       tap(user => {
         if (user) {
           const userDoc = this.db.doc(`user/${user.uid}`);
-          if (this.commonService.selectedAccount !== account.id) {
+          if (this.commonService.selectedAccount.id !== account.id) {
             userDoc.update({ selectedAccount: account })
             .then(() => {
               this.router.navigate(['/alerts', account.id]);
